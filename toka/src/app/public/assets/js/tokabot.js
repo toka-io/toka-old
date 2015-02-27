9a111efe5dd41ebdcedc28f668cacb712f0ab4de
Bob620 <bruder.kraft225@gmail.com>
2015-02-26 22:58:49 -0600

    Added Comments

--- toka/src/app/public/assets/js/tokabot.js
+++ toka/src/app/public/assets/js/tokabot.js
@@ -10,27 +10,26 @@ TokaBot.prototype.parseMessage = function(text) {
 
     textSplit = text.split(' ');
     $domElement = $('<span></span>');
-    console.log(textSplit)
     
+    //Loop over the array of words in the message
     textSplit.forEach(function(Word) {
         run = false
         Word = ' '+Word+' ';
-        console.log(Word);
         
+        //Temp Emote 1
         if (Word == ' Kappa ') {
-            console.log('Kappa');
             run = true;
             $domElement.append($('<img>', {'alt': "Kappa", 'src': "http://174.53.203.111/bobbotconsole/kappa.png", 'height': "26px", 'width': "26px"}));
         };
         
+        //Temp Emote 2
         if (Word == ' OpieOP ') {
-            console.log('OpieOP');
             run = true;
             $domElement.append($('<img>', {'alt': "OpieOP", 'src': "http://174.53.203.111/bobbotconsole/pie.png", 'height': "26px", 'width': "26px"}));
         };
         
+        //If no keywords have been activated, add as plain text
         if (run == false) {
-            console.log('None');
             $domElement.append($('<span></span>').text(Word))
         };
     });
