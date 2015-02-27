d60261b75a5645b851fdd44b3b6429db7026c1e9
Bob620 <bruder.kraft225@gmail.com>
2015-02-26 22:53:22 -0600

    Started TokaBot Message Parsing

--- toka/src/app/public/assets/js/tokabot.js
+++ toka/src/app/public/assets/js/tokabot.js
@@ -7,5 +7,33 @@ function TokaBot() {
 }
 
 TokaBot.prototype.parseMessage = function(text) {
-    text.search("Kappa");
+
+    textSplit = text.split(' ');
+    $domElement = $('<span></span>');
+    console.log(textSplit)
+    
+    textSplit.forEach(function(Word) {
+        run = false
+        Word = ' '+Word+' ';
+        console.log(Word);
+        
+        if (Word == ' Kappa ') {
+            console.log('Kappa');
+            run = true;
+            $domElement.append($('<img>', {'alt': "Kappa", 'src': "http://174.53.203.111/bobbotconsole/kappa.png", 'height': "26px", 'width': "26px"}));
+        };
+        
+        if (Word == ' OpieOP ') {
+            console.log('OpieOP');
+            run = true;
+            $domElement.append($('<img>', {'alt': "OpieOP", 'src': "http://174.53.203.111/bobbotconsole/pie.png", 'height': "26px", 'width': "26px"}));
+        };
+        
+        if (run == false) {
+            console.log('None');
+            $domElement.append($('<span></span>').text(Word))
+        };
+    });
+
+    return $domElement;
 };
