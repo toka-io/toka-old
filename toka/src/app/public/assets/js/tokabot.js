3191b7fde3fabd2df7a0fd191fa003f74ee45d75
Bob620 <bruder.kraft225@gmail.com>
2015-02-27 16:54:47 -0600

    Added Link Parsing

--- toka/src/app/public/assets/js/tokabot.js
+++ toka/src/app/public/assets/js/tokabot.js
@@ -1,35 +1,67 @@
-/* TokaBot
-* @desc: Toka's #1 bot
-*/
+/* TokaBot 1.1
+ * @desc: Toka's #1 bot
+ */
+
 function TokaBot() {
     
-};
+}
 
 TokaBot.prototype.parseMessage = function(text) {
+
     textSplit = text.split(' ');
     $domElement = $('<span></span>');
+    console.log(textSplit)
     
-    //Loop over the array of words in the message
     textSplit.forEach(function(Word) {
+        //Set the regular expresion for links
+        re = /^[\h\t\p\s\:\][a-z0-9\/\ ]+\.[a-z0-9\/\ ]+[\ \.\][a-z0-9\/\ ]+$/i; 
+        var link = [];
         run = false
         Word = ' '+Word+' ';
+        console.log(Word);
         
         //Temp Emote 1
         if (Word == ' Kappa ') {
+            console.log('Kappa');
             run = true;
             $domElement.append($('<img>', {'alt': "Kappa", 'src': "http://174.53.203.111/bobbotconsole/kappa.png", 'height': "26px", 'width': "26px"}));
         };
+        
         //Temp Emote 2
         if (Word == ' OpieOP ') {
+            console.log('OpieOP');
             run = true;
             $domElement.append($('<img>', {'alt': "OpieOP", 'src': "http://174.53.203.111/bobbotconsole/pie.png", 'height': "26px", 'width': "26px"}));
         };
         
-        //If no keywords have been activated, add as plain text
+        //Link Testing via Regular Expresion
+        while ((link = re.exec(Word)) != null) {
+            if (link.index === re.lastIndex) {
+                re.lastIndex++;
+                if (link[0] == Word) {
+                    Pass = false;
+                    if (Word.trim().search('http://') == 0) {
+                        Pass = true;
+                        WordLink = Word.trim();
+                    };
+                    if (Word.trim().search('https://') == 0) {
+                        Pass = true;
+                        WordLink = Word.trim();
+                    };
+                    if (Pass == false) {
+                        WordLink = 'http://'+Word.trim();
+                    }
+                    console.log('Link');
+                    run = true;
+                    $domElement.append($('<a></a>', {'href': WordLink}).text(Word));
+                    break
+                };
+            };
+        };
+        
+        //If it's normal text
         if (run == false) {
-            $domElement.append($('<span></span>').text(Word))
+            console.log('None');
+            $domElement.append($('<span></span>').text(Word));
         };
     });
-    
-    return $domElement;
-};
