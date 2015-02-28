6a06ef234bc1ff8c25c5de7fd395c056d165339e
Bob620 <bruder.kraft225@gmail.com>
2015-02-28 10:46:51 -0600

    Will now fit "use strict"

--- toka/src/app/public/assets/js/tokabot.js
+++ toka/src/app/public/assets/js/tokabot.js
@@ -3,48 +3,49 @@
 * @author: Bob620
 * @revisedBy: ArcTheFallen
 */
+"use strict"
+
 function TokaBot() {
-    
+    //Emote list, {'NAME': 'FILE'}
+    this.emotes = {'Kappa': 'Kappa.png', 'OpieOP': 'pie.png'};
 }
 
 TokaBot.prototype.parseMessage = function(text) {
-    textSplit = text.split(' ');
-    $domElement = $('<span></span>');
-    
-    //Emote list, {'NAME': 'FILE'}
-    emotes = {'Kappa': 'Kappa.png', 'OpieOP': 'pie.png'};
+    var self = this
+    var textSplit = text.split(' ');
+    var $domElement = $('<span></span>');
     
     textSplit.forEach(function(Word) {
-    re = /^[\h\t\s\:\][a-z0-9\/\ ]+\.[a-z0-9\/\ \?\=\#]+[\ \.\][a-z0-9\/\ \?\=\#]+$/i;
-    var link = [];
-    run = false
+        var re = /^[\h\t\s\:\][a-z0-9\/\ ]+\.[a-z0-9\/\ \?\=\#]+[\ \.\][a-z0-9\/\ \?\=\#]+$/i; 
+        var link = [];
+        var run = false;
     
-    //emotes that are grabbed from the emote list
-    if (emotes.hasOwnProperty(Word)) {
-        run = true;
-        $domElement.append($('<img>', {'title': Word, 'alt': Word, 'src': "http://toka.io/assets/images/emotes/"+emotes[Word], 'height': "26px"}));
-    };
-    
-    //Link logic
+        //emotes that are grabbed from the emote list
+        if (self.emotes.hasOwnProperty(Word)) {
+            run = true;
+            $domElement.append($('<img>', {'title': Word, 'alt': Word, 'src': "http://toka.io/assets/images/emotes/"+self.emotes[Word], 'height': "26px"}));
+        };
+        
+        //Link logic
         while ((link = re.exec(Word)) != null) {
             if (link.index === re.lastIndex) {
                 re.lastIndex++;
                 if (link[0] == Word) {
-                    Pass = false;
+                    var Pass = false;
                     if (Word.search('http://') == 0) {
                         Pass = true;
-                        WordLink = Word;
+                        var WordLink = Word;
                     };
                     if (Word.search('https://') == 0) {
                         Pass = true;
-                        WordLink = Word;
+                        var WordLink = Word;
                     };
                     if (Pass == false) {
-                        WordLink = 'http://'+Word;
+                        var WordLink = 'http://'+Word;
                     };
                     run = true;
                     $domElement.append($('<a></a>', {'href': WordLink, 'target': '_blank'}).text(' '+Word+' '));
-                    break
+                    break;
                 };
             };
         };
