42f4c4ab2b9606c649c9017b5f795de43bf95930
Andy Lim <andytlim@gmail.com>
2015-08-10 01:23:27 -0400

    Updated password recovery page. Added a universal alert popup.

---
+++ toka/src/app/public/view/common/alert.php
@@ -0,0 +1,8 @@
+<!--  Alert Popup -->
+<div class="modal fade" id="site-alert-popup" tabindex="-1" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <h3 class="message"></h3>
+        </div>
+    </div>
+</div>
