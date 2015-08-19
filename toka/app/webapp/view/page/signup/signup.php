eb55d05e4f3ae2be9946747be0ace75be43567a2
Andy Lim <andytlim@gmail.com>
2015-08-18 21:54:30 -0400

    Updated

--- toka/app/webapp/view/page/signup/signup.php
+++ toka/app/webapp/view/page/signup/signup.php
@@ -53,9 +53,11 @@
             <section class="alert-container">
                 <div class="alert <?php echo (!empty($response)) ? 'alert-info' : '' ?>"><span>
                     <?php 
-                    if (!empty($response) && $response['status'] === "0") {
-                        echo ucfirst($response['statusMsg']) . '.'; 
-                    } else 
+                    if (!empty($response) && $response['status'] === "0")
+                        echo ucfirst($response['statusMsg']) . '.';
+                    else if (!empty($response) && $response['status'] === "1")
+                        echo $response['displayMsg'];
+                    else 
                         echo '&nbsp;';
                     ?>
                 </span></div>
