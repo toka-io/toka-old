176336cce437f1f88d035b49f288bd388a71a653
Andy Lim <andytlim@gmail.com>
2015-10-19 19:49:07 -0400

    Fixed repo

--- app/src/resource/config.php
+++ app/src/resource/config.php
@@ -5,7 +5,7 @@ return array(
             "username" => "toka",
             "password" => "Mir@c!3!23"
        ),
-       "primary" => "mongodb://ds055021-a1.mongolab.com:55021/toka",
+       "primary" => "mongodb://ds055021-a0.mongolab.com:55021,ds055021-a1.mongolab.com:55021/toka?replicaSet=rs-ds055021",
        "replicaSet" => "mongodb://ds055021-a0.mongolab.com:55021,ds055021-a1.mongolab.com:55021/toka"       
    ),
    "cloudinary" => array(
